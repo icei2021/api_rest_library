5ee134fb3755c48195681187fde4559e5e6de09b
Arnaldo Muñoz <kings31lendy@gmail.com>
2023-09-08 13:41:43 -0400

    agregando cors IMPORTANTE

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const app = express();
 const connectDatabase = require("./config/db");
 const dotenv = require("dotenv");
 const booksRoutes = require("./routes/Book.route");
+const cors = require("cors");
 
 dotenv.config({path:"./config/config.env"})
 //Llamando a nuestra conexión
 connectDatabase();
 app.use(express.json());
 app.use("/api/book", booksRoutes)
-
+app.use(cors());
 const server = app.listen(process.env.PORT, ()=>{
     console.log("Connecting to the PORT "+ process.env.PORT);
 });
